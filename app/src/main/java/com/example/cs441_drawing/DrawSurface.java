c3217cf9beda2817d97a9db0a40eb776ff53c89b
Kevin Wallace <kjwall97@yahoo.com>
2019-09-13 10:30:30 -0400

    Scrapped project, inserted sample code to start from for next time

--- app/src/main/java/com/example/cs441_drawing/DrawSurface.java
+++ app/src/main/java/com/example/cs441_drawing/DrawSurface.java
@@ -4,112 +4,11 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.view.MotionEvent;
 import android.view.SurfaceView;
 import android.view.SurfaceHolder;
-
-import com.example.cs441_drawing.R;
+import android.view.View;
 
 public class DrawSurface extends SurfaceView implements SurfaceHolder.Callback {
-    private SurfaceHolder surfaceHolder = null;
-    private Paint paint = null;
-    private int brushColor = 0;
-    private int brushRadius = 0;
-    private float circleX = 0;
-    private float circleY = 0;
-
-    public DrawSurface(Context context) {
-        super(context);
-        surfaceHolder = getHolder();
-
-        paint = new Paint();
-        paint.setColor(getResources().getColor(R.color.DEFAULT_COLOR));
-    }
-
-    public DrawSurface(Context context, int inkColor, int inkRadius) {
-        super(context);
-        surfaceHolder = getHolder();
-
-        paint = new Paint();
-        paint.setColor(getResources().getColor(R.color.DEFAULT_COLOR));
-        brushColor = inkColor;
-        brushRadius = inkRadius;
-
-    }
-
-    @Override
-    public void surfaceCreated(SurfaceHolder surfaceHolder) {
-        drawBall();
-    }
-
-    @Override
-    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
-
-    }
-
-    @Override
-    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-        paint = null;
-    }
-
-    public void drawBall() {
-        surfaceHolder = getHolder();
-
-        Canvas canvas = surfaceHolder.lockCanvas();
-
-        Paint surfaceBackground = new Paint();
-
-        surfaceBackground.setColor(Color.TRANSPARENT);
-
-        canvas.drawRect(0,0,this.getWidth(), this.getHeight(), surfaceBackground);
-
-        paint.setColor(brushColor);
-        canvas.drawCircle(circleX, circleY, brushRadius, paint);
-
-        //Why is this here? see if it can be removed
-        canvas.drawCircle(50, 50, 200, paint);
-
-        surfaceHolder.unlockCanvasAndPost(canvas);
-    }
-
-
-    public float getCircleX() {
-        return circleX;
-    }
-
-    public void setCircleX(float circleX) {
-        this.circleX = circleX;
-    }
-
-    public float getCircleY() {
-        return circleY;
-    }
-
-    public void setCircleY(float circleY) {
-        this.circleY = circleY;
-    }
-
-    public Paint getPaint() {
-        return paint;
-    }
-
-    public void setPaint(Paint paint) {
-        this.paint = paint;
-    }
-
-    public int getColor() {
-        return brushColor;
-    }
-
-    public void setColor(int newColor) {
-        brushColor = newColor;
-    }
-
-    public int getRadius() {
-        return brushRadius;
-    }
-
-    public void setRadius(int radius) {
-        brushRadius = radius;
-    }
 
 }
--- app/src/main/java/com/example/cs441_drawing/MainActivity.java
+++ app/src/main/java/com/example/cs441_drawing/MainActivity.java
@@ -3,68 +3,127 @@ package com.example.cs441_drawing;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
+
 public class MainActivity extends AppCompatActivity {
-    private Button eraseButton = null;
-    private Button drawButton = null;
-    private boolean drawing = true;
-    private int brushSize = 25;
-    private int brushColor = 0;
+    private Button redButton = null;
+
+    private Button greenButton = null;
+
+    private boolean drawBall = true;
+
     private LinearLayout canvasLayout = null;
 
-    DrawSurface drawSurfaceView = null;
-
+    DrawSurface customSurfaceView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setTitle("CS441_Drawing");
-        // Actual code down here
+        setTitle("SurfaceView");
 
         initControls();
 
-        // Hide app title bar (FROM SAMPLE)
+        // Hide the app title bar.
         getSupportActionBar().hide();
 
-        // Make app fullscreen (FROM SAMPLE)
+        // Make app full screen to hide top status bar.
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
-        drawSurfaceView = new DrawSurface(getApplicationContext());
+        // Create custom surfaceview object.
+        customSurfaceView = new DrawSurface(getApplicationContext());
 
-        drawSurfaceView.setOnTouchListener((View.OnTouchListener) this);
+        // Set this as the onTouchListener to process custom surfaceview ontouch event.
+        //customSurfaceView.setOnTouchListener(this);
 
-        canvasLayout.addView(drawSurfaceView);
+        // Add the custom surfaceview object to the layout.
+        canvasLayout.addView(customSurfaceView);
 
-        eraseButton.setOnClickListener(new View.OnClickListener() {
+        // Click this button to draw a red circle ball move after finger touch.
+        redButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                drawing = false;
-                drawSurfaceView.setColor(Color.TRANSPARENT);
+                drawBall = true;
             }
         });
 
-        drawButton.setOnClickListener(new View.OnClickListener() {
-          @Override
-          public void onClick(View view) {
-              drawing = true;
-              drawSurfaceView.setColor(brushColor);
-          }
+        // Click this button to draw a green rectangle move after finger touch.
+        greenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawBall = false;
+            }
         });
+
     }
 
-    private void initControls() {
-        if(eraseButton == null)
-            eraseButton = findViewById(R.id.eraseButton);
-        if(drawButton == null)
-            drawButton = findViewById(R.id.drawButton);
-        if(canvasLayout == null)
-            canvasLayout = findViewById(R.id.canvasLayout);
+    /* Initialise ui controls. */
+    private void initControls()
+    {
+        if(redButton == null)
+        {
+            redButton = findViewById(R.id.redButton);
+        }
+
+        if(greenButton == null)/* If user finger touch the surfaceview object. */
+            @Override
+        public boolean onTouchEvent(View view, MotionEvent motionEvent) {
+
+        // If user touch the custom SurfaceView object.
+        if(view instanceof SurfaceView) {
+            view.invalidate();
+
+            float x = motionEvent.getX();
+
+            float y = motionEvent.getY();
+
+            customSurfaceView.setCircleX(x);
+
+            customSurfaceView.setCircleY(y);
+
+            if (drawBall) {
+                // Create and set a red paint to custom surfaceview.
+                Paint paint = new Paint();
+                paint.setColor(Color.RED);
+                customSurfaceView.setPaint(paint);
+
+                customSurfaceView.drawBall();
+            } else {
+                // Create and set a green paint to custom surfaceview.
+                Paint paint = new Paint();
+                paint.setColor(Color.GREEN);
+                customSurfaceView.setPaint(paint);
+
+                customSurfaceView.drawRect();
+            }
+
+            // Tell android os the onTouch event has been processed.
+            return true;
+        }else
+        {
+            // Tell android os the onTouch event has not been processed.
+            return false;
+        }
     }
+        {
+            greenButton = findViewById(R.id.greenButton);
+        }
+
+        // This layout is used to contain custom surfaceview object.
+        if(canvasLayout == null)
+        {
+            canvasLayout = findViewById(R.id.customViewLayout);
+        }
+    }
+
+
 }
