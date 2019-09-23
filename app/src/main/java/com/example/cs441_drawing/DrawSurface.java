f9b3db09a5bccb587604c067ecb3af6925fbb1c0
Kevin Wallace <kjwall97@yahoo.com>
2019-09-22 21:01:03 -0400

    3 Lines can now be independently adjusted for angle and y-intercept. Added displays and UI to facilitate all of this

--- app/src/main/java/com/example/cs441_drawing/DrawSurface.java
+++ app/src/main/java/com/example/cs441_drawing/DrawSurface.java
@@ -10,12 +10,14 @@ import android.view.SurfaceHolder;
 public class DrawSurface extends SurfaceView implements SurfaceHolder.Callback {
     public SurfaceHolder surfaceHolder = null;
     private Paint paint = null;
-    private float m, b, d;
+    private float[] b = {0, 0, 0};
+    private float[] m = {0, 0, 0};
+    private int[] colors = {Color.RED, Color.BLUE, Color.GREEN};
+    private float d;
 
     public DrawSurface(Context context) {
         super(context);
         paint = new Paint();
-        paint.setColor(Color.RED);
     }
     @Override
     public void surfaceCreated(SurfaceHolder surfaceHolder) {
@@ -36,34 +38,40 @@ public class DrawSurface extends SurfaceView implements SurfaceHolder.Callback {
     public void draw()
     {
         surfaceHolder = getHolder();
-
         // Get and lock canvas object from surfaceHolder.
         Canvas canvas = surfaceHolder.lockCanvas();
-
         Paint surfaceBackground = new Paint();
-        // Set the surfaceview background color.
+        // Set the surfaceview background color
         surfaceBackground.setColor(Color.BLACK);
         // Draw the surfaceview background color.
         canvas.drawRect(0, 0, this.getWidth(), this.getHeight(), surfaceBackground);
-
-        // Draw the circle.
-        paint.setColor(Color.RED);
+        // Draw the line.
         d = (float)Math.sqrt(Math.pow(this.getHeight()*2, 2) + Math.pow(this.getWidth()*2, 2));
-        float startX = (float)((this.getWidth() / 2) - d + (d - Math.cos(m)*d));
-        float startY = (float)((this.getHeight() / 2) - d + (d - Math.sin(m)*d));
-        float stopX = (float)((this.getWidth() / 2) + d - (d - Math.cos(m)*d));
-        float stopY = (float)((this.getHeight() / 2) + d - (d - Math.sin(m)*d));
-        canvas.drawLine(startX, startY + b, stopX, stopY + b, paint);
+        for(int i=0; i<m.length; i++) {
+            paint.setColor(colors[i]);
+            float startX = (float) ((this.getWidth() / 2) - d + (d - Math.cos(m[i]) * d));
+            float startY = (float) (b[i] - d + (d - Math.sin(m[i]) * d));
+            float stopX = (float) ((this.getWidth() / 2) + d - (d - Math.cos(m[i]) * d));
+            float stopY = (float) (b[i] + d - (d - Math.sin(m[i]) * d));
+            canvas.drawLine(startX, startY, stopX, stopY, paint);
+        }
         // Unlock the canvas object and post the new draw.
         surfaceHolder.unlockCanvasAndPost(canvas);
     }
 
 
-    public void setM(float m) {
-        this.m = (float)(2*Math.PI*m / 100);
+    public void setM(float m, int index) {
+        this.m[index] = (float)(Math.PI*m / 100);
     }
-    public void setB(float b) {
-        this.b = b * this.getHeight() / 100;
+    public void setB(float b, int index) {
+        this.b[index] = b * this.getHeight() / 100;
     }
 
+    public float getM(int index) {
+        return this.m[index];
+    }
+
+    public float getB(int index) {
+        return this.b[index];
+    }
 }
--- app/src/main/java/com/example/cs441_drawing/MainActivity.java
+++ app/src/main/java/com/example/cs441_drawing/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.cs441_drawing;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -10,7 +13,13 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     private SeekBar slopeBar = null;
     private SeekBar interceptBar = null;
+    private TextView activeText = null;
+    private TextView slopeText = null;
+    private TextView bText = null;
     private DrawSurface customGraph = null;
+    private int activeLine = 0;
+    private int m[] = {0,0,0};
+    private int b[] = {0,0,0};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,9 +45,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress,boolean fromUser) {
                 // TODO Auto-generated method stub
-                customGraph.setM(progress);
-                TextView test = findViewById(R.id.testText);
-                test.setText(Integer.toString(progress));
+                customGraph.setM(progress, activeLine);
+                m[activeLine] = progress;
+                slopeText.setText(String.format("Angle:\n%.2f", customGraph.getM(activeLine)));
                 customGraph.draw();
             }
         });
@@ -58,11 +67,56 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress,boolean fromUser) {
                 // TODO Auto-generated method stub
-                customGraph.setB(progress);
-                TextView test = findViewById(R.id.testText);
-                test.setText(Integer.toString(progress));
+                customGraph.setB(progress, activeLine);
+                b[activeLine] = progress;
+                bText.setText(String.format("Y-Intercept:\n%.2f", customGraph.getB(activeLine)));
                 customGraph.draw();
             }
         });
+
+        activeText = findViewById(R.id.activeText);
+        slopeText = findViewById(R.id.slopeText);
+        bText = findViewById(R.id.bText);
+        Button redButton = findViewById(R.id.redButton);
+        redButton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               activeLine = 0;
+               slopeBar.setProgress(m[activeLine]);
+               interceptBar.setProgress(b[activeLine]);
+               activeText.setText("Active:\nRed");
+               slopeText.setText(String.format("Angle:\n%.2f", customGraph.getM(activeLine)));
+               bText.setText(String.format("Y-Intercept:\n%.2f", customGraph.getB(activeLine)));
+
+           }
+        });
+
+        Button blueButton = findViewById(R.id.blueButton);
+        blueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                activeLine = 1;
+                slopeBar.setProgress(m[activeLine]);
+                interceptBar.setProgress(b[activeLine]);
+                activeText.setText("Active:\nBlue");
+                slopeText.setText(String.format("Angle:\n%.2f", customGraph.getM(activeLine)));
+                bText.setText(String.format("Y-Intercept:\n%.2f", customGraph.getB(activeLine)));
+
+            }
+        });
+
+        Button greenButton = findViewById(R.id.greenButton);
+        greenButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                activeLine = 2;
+                slopeBar.setProgress(m[activeLine]);
+                interceptBar.setProgress(b[activeLine]);
+                activeText.setText("Active:\nGreen");
+                slopeText.setText(String.format("Angle:\n%.2f", customGraph.getM(activeLine)));
+                bText.setText(String.format("Y-Intercept:\n%.2f", customGraph.getB(activeLine)));
+            }
+        });
     }
+
 }
